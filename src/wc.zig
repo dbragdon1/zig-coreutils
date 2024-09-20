b2985f7765607e4325a88e5b7126843800b000ee
Dylan Bragdon <dylan.bragdon@gmail.com>
2024-09-19 22:18:05 -0700

    refactored wc counting into separate function

--- src/wc.zig
+++ src/wc.zig
@@ -2,37 +2,7 @@ const std = @import("std");
 
 test "count lines" {}
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
-    const allocator = gpa.allocator();
-
-    var args = try std.process.argsWithAllocator(allocator);
-
-    defer args.deinit();
-
-    _ = args.skip(); // skip the program name
-
-    var arg_count: usize = 0;
-    var filename: []const u8 = "";
-
-    while (args.next()) |arg| {
-        filename = arg;
-        arg_count += 1;
-        break;
-    }
-
-    var reader: std.fs.File.Reader = undefined;
-
-    if (arg_count == 0) {
-        const stdin = std.io.getStdIn();
-        reader = stdin.reader();
-    } else {
-        var file = try std.fs.cwd().openFile(filename, .{});
-        //defer file.close();
-        reader = file.reader();
-    }
-
+pub fn count(reader: std.fs.File.Reader, filename: []const u8) !void {
     var buffer: [4096]u8 = undefined;
 
     var wc: usize = 0; // word count
@@ -59,4 +29,38 @@ pub fn main() !void {
     }
 
     std.debug.print(" {d} {d} {d} {s}\n", .{ lc, wc, bc, filename });
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+    const allocator = gpa.allocator();
+
+    var args = try std.process.argsWithAllocator(allocator);
+
+    defer args.deinit();
+
+    _ = args.skip(); // skip the program name
+
+    var arg_count: usize = 0;
+    var filename: []const u8 = "";
+
+    while (args.next()) |arg| {
+        filename = arg;
+        arg_count += 1;
+        break;
+    }
+
+    var reader: std.fs.File.Reader = undefined;
+
+    if (arg_count == 0) {
+        const stdin = std.io.getStdIn();
+        reader = stdin.reader();
+        try count(reader, "-");
+    } else {
+        var file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
+        reader = file.reader();
+        try count(reader, filename);
+    }
 }
